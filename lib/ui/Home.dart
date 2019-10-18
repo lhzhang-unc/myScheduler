9c5f6ec774010af548ab220243dd2a3143a4e2f5
lucas <lhzhang@live.unc.edu>
2019-10-17 23:23:49 -0400

    Fixed glitches in the friday timeline caused by the placeholder schedule

--- lib/ui/Home.dart
+++ lib/ui/Home.dart
@@ -1,5 +1,4 @@
 import 'dart:core';
-import 'dart:core';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:scheduler/util/Classes.dart';
@@ -11,27 +10,30 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  PageController _pageController = new PageController(
+      initialPage: DateTime.now().weekday < 5 ? DateTime.now().weekday - 1 : 0,
+      keepPage: true);
 
-  PageController _pageController = new PageController(initialPage: DateTime.now().weekday < 5 ? DateTime.now().weekday - 1 : 0, keepPage: true);
   @override
   Widget build(BuildContext context) {
-      return Scaffold(
-        appBar: new AppBar(
-          title: Text("myScheduler", style: new TextStyle(
-              color: Colors.black, fontSize: 35
-          ),),
-          backgroundColor: Colors.white,
+    return Scaffold(
+      appBar: new AppBar(
+        title: Text(
+          "myScheduler",
+          style: new TextStyle(color: Colors.black, fontSize: 35),
         ),
-        body: PageView(
-          controller: _pageController,
-          children: <Widget>[
-            TimeLine(MondayClasses.eventList),
-            TimeLine(TuesdayClasses.eventList),
-            TimeLine(WednesdayClasses.eventList),
-            TimeLine(ThursdayClasses.eventList),
-            TimeLine(FridayClasses.eventList),
-          ],
-        ),
+        backgroundColor: Colors.white,
+      ),
+      body: PageView(
+        controller: _pageController,
+        children: <Widget>[
+          TimeLine(MondayClasses.eventList),
+          TimeLine(TuesdayClasses.eventList),
+          TimeLine(WednesdayClasses.eventList),
+          TimeLine(ThursdayClasses.eventList),
+          TimeLine(FridayClasses.eventList),
+        ],
+      ),
     );
   }
 }
--- lib/util/Classes.dart
+++ lib/util/Classes.dart
@@ -11,7 +11,7 @@ class MondayClasses {
   static var _class6 = new ClassEvent("ENGL 105-024", 50, true, "2:30 pm - 3:20 pm");
   static var _class7 = new ClassEvent("", 520, false, "");
   static var _class8 = new ClassEvent("", 120, false, ""); //Padding
-
+  //Passes list of items to timeline
   static get eventList => [_class0, _class1, _class2, _class3, _class4, _class5, _class6, _class7, _class8];
 }
 
@@ -60,9 +60,9 @@ class FridayClasses {
   //Dummy information
   static var _class0 = new ClassEvent("", 120, false, ""); //Padding
   static var _class1 = new ClassEvent("", 545, false, "");
-  static var _class2 = new ClassEvent("PHYS 118-002 Q&A", 50, true, "9:05 am - 9:55 am");
+  static var _class2 = new ClassEvent("PHYS 118-002", 50, true, "9:05 am - 9:55 am");
   static var _class3 = new ClassEvent("", 145, false, "");
-  static var _class4 = new ClassEvent("COMP 401 Recitation", 50, true, "12:20 pm - 1:10 pm");
+  static var _class4 = new ClassEvent("COMP 401-605", 50, true, "12:20 pm - 1:10 pm");
   static var _class5 = new ClassEvent("", 80, false, "");
   static var _class6 = new ClassEvent("ENGL 105-024", 50, true, "2:30 pm - 3:20 pm");
   static var _class7 = new ClassEvent("", 520, false, "");
